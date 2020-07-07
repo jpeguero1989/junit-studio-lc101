500b83eacf5840ad5f9a62263c1b439b78bed748
Jhoan Peguero <jpeguero1989@gmail.com>
2020-07-06 23:02:28 -0400

    Ready

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,66 @@ public class BalancedBracketsTest {
     @Test
     public void emptyTest() {
         assertEquals(true, true);
+    }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void noBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+    @Test
+    public void bracketsInMidleReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[code]"));
+    }
+
+    @Test
+    public void stringWithNoBracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch"));
+    }
+
+    @Test
+    public void bracketsWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[hello]"));
+    }
+
+    @Test
+    public void bracketsAtFirstWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]hello"));
+    }
+
+    @Test
+    public void bracketsAtEndWithTextReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("hello[]"));
+    }
+
+    @Test
+    public void oneBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void oneBracketsWithTextReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]bnb"));
+    }
+
+    @Test
+    public void misplacedBracketsReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void misplacedBracketsAtEndWithStringReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("LaunchCode]["));
+    }
+
+    @Test
+    public void misplacedBracketsAtFirstWithStringReturnFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][LaunchCode"));
     }
 
 
